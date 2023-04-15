a3bb0c26909f32f7732e8ae446eeb2bc1389b630
egisb <egidijusb@gmail.com>
2023-04-15 09:31:12 +0300

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,9 @@ if defined?(Bundler)
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
-
+module ActionController::HttpAuthentication::Token
+  AUTHN_PAIR_DELIMITERS = /(?:,|;|\t)/
+end
 module DemoApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
